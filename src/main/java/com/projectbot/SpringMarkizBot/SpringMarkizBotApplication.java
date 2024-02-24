94bae46e29403c900e912dfec289006928f00a5c
Your Name <azzytm@yandex.ru>
2024-02-24 23:08:36 +0300

    update

---
+++ src/main/java/com/projectbot/SpringMarkizBot/SpringMarkizBotApplication.java
@@ -0,0 +1,13 @@
+package com.projectbot.SpringMarkizBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMarkizBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMarkizBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectbot/SpringMarkizBot/SpringMarkizBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectbot.SpringMarkizBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMarkizBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
