3d0887c3d29e0cfeef15f2111f35ff1765a7dfb3
Your Name <azzytm@yandex.ru>
2024-02-27 21:03:56 +0300

    вынес в отдельный метод InlineKeyboardMarkup

--- src/main/java/com/projectbot/SpringMarkizBot/service/TelegramBot.java
+++ src/main/java/com/projectbot/SpringMarkizBot/service/TelegramBot.java
@@ -5,9 +5,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -42,7 +40,6 @@ public class TelegramBot extends TelegramLongPollingBot {
            String messageText = update.getMessage().getText();
            long chatId = update.getMessage().getChatId();
 
-
            switch (messageText) {
                case "/start":
 
@@ -70,34 +67,19 @@ public class TelegramBot extends TelegramLongPollingBot {
     private void helpCommandLine(Long chatId) {
         String helpText = "/start - начало работы\n/help - команды бота";
 
-
         sendMessage(chatId, helpText);
     }
 
     private void startCommandReceived(long chatId, String name) {
 
-       String answer = "Hi, " + name + ", nice to meet you";
+       String answer = "Привет, " + name + " , этот бот шаблон для будущих ботов !";
        log.info("Replied to user: " + name);
 
        sendMessage(chatId, answer);
     }
 
-    private void answerCallbackQuery(String callbackQueryId) {
-        AnswerCallbackQuery answerCallbackQuery = new AnswerCallbackQuery();
-        answerCallbackQuery.setCallbackQueryId(callbackQueryId);
-        try {
-            execute(answerCallbackQuery);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private void sendMessage(Long chatId, String textToSend) {
-
-        SendMessage message = new SendMessage();
-        message.setChatId(chatId.toString());
-        message.setText(textToSend);
-
+    private static InlineKeyboardMarkup getInlineKeyboardMarkup() {
         List<InlineKeyboardButton> keyboardButtonRow1 = new ArrayList<>();
 
         InlineKeyboardButton button1 = new InlineKeyboardButton();
@@ -111,15 +93,23 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
         inlineKeyboardMarkup.setKeyboard(keyboard);
+        return inlineKeyboardMarkup;
+    }
+
+    private void sendMessage(Long chatId, String textToSend) {
+
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId.toString());
+        message.setText(textToSend);
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = getInlineKeyboardMarkup();
 
         message.setReplyMarkup(inlineKeyboardMarkup);
-
 
         try {
             execute(message);
         }
         catch (TelegramApiException e) {
-            e.printStackTrace();
             log.error("Error occurred: " + e.getMessage());
         }
     }
