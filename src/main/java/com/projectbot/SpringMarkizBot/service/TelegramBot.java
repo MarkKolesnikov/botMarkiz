e20a6e80c3128262e0a627be092924dbc3f542c2
Your Name <azzytm@yandex.ru>
2024-02-27 18:30:35 +0300

    добавлена inline панель для кнопок в боте

--- src/main/java/com/projectbot/SpringMarkizBot/service/TelegramBot.java
+++ src/main/java/com/projectbot/SpringMarkizBot/service/TelegramBot.java
@@ -5,9 +5,16 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -35,16 +42,36 @@ public class TelegramBot extends TelegramLongPollingBot {
            String messageText = update.getMessage().getText();
            long chatId = update.getMessage().getChatId();
 
+
            switch (messageText) {
                case "/start":
 
                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                        break;
+               case "/help":
+
+                       helpCommandLine(chatId);
+                       break;
+
                default:
 
-                       sendMessage(chatId, "Sorry :( ");
+                       sendMessage(chatId, "Я не понимаю твоей команды! ");
+           }
+       } else if (update.hasCallbackQuery()) {
+           String callbackData = update.getCallbackQuery().getData();
+           Long chatId = update.getCallbackQuery().getMessage().getChatId();
+
+           if ("help".equals(callbackData)) {
+               helpCommandLine(chatId);
            }
        }
+    }
+
+    private void helpCommandLine(Long chatId) {
+        String helpText = "/start - начало работы\n/help - команды бота";
+
+
+        sendMessage(chatId, helpText);
     }
 
     private void startCommandReceived(long chatId, String name) {
@@ -55,16 +82,44 @@ public class TelegramBot extends TelegramLongPollingBot {
        sendMessage(chatId, answer);
     }
 
+    private void answerCallbackQuery(String callbackQueryId) {
+        AnswerCallbackQuery answerCallbackQuery = new AnswerCallbackQuery();
+        answerCallbackQuery.setCallbackQueryId(callbackQueryId);
+        try {
+            execute(answerCallbackQuery);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void sendMessage(Long chatId, String textToSend) {
 
         SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
+        message.setChatId(chatId.toString());
         message.setText(textToSend);
+
+        List<InlineKeyboardButton> keyboardButtonRow1 = new ArrayList<>();
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton();
+        button1.setText("Посмотреть команды");
+        button1.setCallbackData("help");
+
+        keyboardButtonRow1.add(button1);
+
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+        keyboard.add(keyboardButtonRow1);
+
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        inlineKeyboardMarkup.setKeyboard(keyboard);
+
+        message.setReplyMarkup(inlineKeyboardMarkup);
+
 
         try {
             execute(message);
         }
         catch (TelegramApiException e) {
+            e.printStackTrace();
             log.error("Error occurred: " + e.getMessage());
         }
     }
